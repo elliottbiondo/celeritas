ee79996517b92703a0538cac80ce250a957f96e8
Elliott Biondo <biondoed@ornl.gov>
2024-11-01 20:29:40 -0400

    Start BIHIntersectingVolFinder

---
+++ src/orange/detail/BIHIntersectingVolFinder.hh
@@ -0,0 +1,260 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2022-2024 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file orange/detail/BIHIntersectingVolFinder.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "corecel/math/Algorithms.hh"
+
+#include "BIHTraversalHelper.hh"
+#include "../BoundingBoxUtils.hh"
+#include "../OrangeData.hh"
+#include "../OrangeTypes.hh"
+
+namespace celeritas
+{
+namespace detail
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Traverse BIH tree using a depth-first search.
+ *
+ * \todo move to top-level orange directory out of detail namespace
+ */
+class BIHIntersectingVolFinder
+{
+  public:
+    //!@{
+    //! \name Type aliases
+    using Storage = NativeCRef<BIHTreeData>;
+
+    struct Ray
+    {
+        Real3 pos;
+        Real3 dir;
+    };
+
+    struct Intersection
+    {
+        LocalVolumeId vol_id;
+        double dist;
+    };
+
+    //!@}
+
+    // Construct from vector of bounding boxes and storage for LocalVolumeIds
+    inline CELER_FUNCTION
+    BIHIntersectingVolFinder(BIHTree const& tree, Storage const& storage);
+
+    // Point-in-volume operation
+    template<class F>
+    inline CELER_FUNCTION Intersection operator()(Ray const& ray,
+                                                  F&& visit_vol) const;
+
+  private:
+    //// DATA ////
+    BIHTree const& tree_;
+    Storage const& storage_;
+    BIHTraversalHelper helper_;
+
+    //// HELPER FUNCTIONS ////
+
+    // Get the ID of the next node in the traversal sequence
+    inline CELER_FUNCTION BIHNodeId next_node(BIHNodeId const& current_id,
+                                              BIHNodeId const& previous_id,
+                                              Ray const& ray,
+                                              double min_dist) const;
+
+    // Determine if an edge or volume bbox
+    inline CELER_FUNCTION bool
+    visit_bbox(FastBBox const& bbox, Ray const& ray, double min_dist) const;
+
+    // Determine if any leaf node volumes contain the point
+    template<class F>
+    inline CELER_FUNCTION Intersection visit_leaf(BIHLeafNode const& leaf_node,
+                                                  Ray const& ray,
+                                                  Intersection intersection,
+                                                  F&& visit_vol) const;
+
+    // Determine if any inf_vols contain the point
+    template<class F>
+    inline CELER_FUNCTION Intersection
+    visit_inf_vols(F&& is_inside, Intersection intersection) const;
+};
+
+//---------------------------------------------------------------------------//
+// INLINE DEFINITIONS
+//---------------------------------------------------------------------------//
+/*!
+ * Construct from vector of bounding boxes and storage.
+ */
+CELER_FUNCTION
+BIHIntersectingVolFinder::BIHIntersectingVolFinder(
+    BIHTree const& tree, BIHIntersectingVolFinder::Storage const& storage)
+    : tree_(tree), storage_(storage), helper_(tree, storage)
+{
+    CELER_EXPECT(tree);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Point-in-volume operation.
+ */
+template<class F>
+CELER_FUNCTION auto
+BIHIntersectingVolFinder::operator()(BIHIntersectingVolFinder::Ray const& ray,
+                                     F&& visit_vol) const -> Intersection
+{
+    BIHNodeId previous_node;
+    BIHNodeId current_node{0};
+
+    Intersection intersection{LocalVolumeId{},
+                              std::numeric_limits<real_type>::infinity()};
+
+    do
+    {
+        if (!helper_.is_inner(current_node))
+        {
+            intersection = this->visit_leaf(helper_.get_leaf_node(current_node),
+                                            ray,
+                                            intersection,
+                                            visit_vol);
+        }
+
+        previous_node = exchange(
+            current_node,
+            this->next_node(
+                current_node, previous_node, ray, intersection.dist));
+
+    } while (current_node);
+
+    // return this->visit_inf_vols(is_inside);
+
+    return intersection;
+}
+
+//---------------------------------------------------------------------------//
+// HELPER FUNCTIONS
+//---------------------------------------------------------------------------//
+/*!
+ *  Get the ID of the next node in the traversal sequence.
+ */
+CELER_FUNCTION
+BIHNodeId BIHIntersectingVolFinder::next_node(BIHNodeId const& current_id,
+                                              BIHNodeId const& previous_id,
+                                              Ray const& ray,
+                                              double min_dist) const
+{
+    using Side = BIHInnerNode::Side;
+
+    BIHNodeId next_id;
+
+    if (helper_.is_inner(current_id))
+    {
+        auto const& current_node = helper_.get_inner_node(current_id);
+        auto const& l_edge = current_node.edges[Side::left];
+        auto const& r_edge = current_node.edges[Side::right];
+
+        if (previous_id == current_node.parent)
+        {
+            // Visiting this inner node for the first time; go down either left
+            // or right edge
+            next_id = this->visit_bbox(l_edge.bbox, ray, min_dist)
+                          ? l_edge.child
+                          : r_edge.child;
+        }
+        else if (previous_id == current_node.edges[Side::left].child)
+        {
+            // Visiting this inner node for the second time; go down right edge
+            // or return to parent
+            next_id = this->visit_bbox(r_edge.bbox, ray, min_dist)
+                          ? r_edge.child
+                          : current_node.parent;
+        }
+        else
+        {
+            // Visiting this inner node for the third time; return to parent
+            CELER_EXPECT(previous_id == current_node.edges[Side::right].child);
+            next_id = current_node.parent;
+        }
+    }
+    else
+    {
+        // Leaf node; return to parent
+        CELER_EXPECT(previous_id == helper_.get_leaf_node(current_id).parent);
+        next_id = previous_id;
+    }
+
+    return next_id;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Determine if traversal shall proceed down a given edge.
+ */
+CELER_FUNCTION
+bool BIHIntersectingVolFinder::visit_bbox(FastBBox const& bbox,
+                                          Ray const& ray,
+                                          double min_dist) const
+{
+    return is_inside(bbox, ray.pos)
+           || calc_dist_to_inside(bbox, ray.pos, ray.dir) < min_dist;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Determine if any leaf node volumes contain the point.
+ */
+template<class F>
+CELER_FUNCTION auto BIHIntersectingVolFinder::visit_leaf(
+    BIHLeafNode const& leaf_node,
+    BIHIntersectingVolFinder::Ray const& ray,
+    BIHIntersectingVolFinder::Intersection min_intersection,
+    F&& visit_vol) const -> Intersection
+{
+    for (auto i : range(leaf_node.vol_ids.size()))
+    {
+        auto id = storage_.local_volume_ids[leaf_node.vol_ids[i]];
+
+        auto const& bbox = storage_.bboxes[tree_.bboxes[id]];
+
+        if (this->visit_bbox(bbox, ray, min_intersection.dist))
+        {
+            auto intersection = visit_vol(id);
+            if (intersection.dist < min_intersection.dist)
+            {
+                min_intersection = intersection;
+            }
+        }
+    }
+    return min_intersection;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Determine if any volumes in inf_vols contain the point.
+ */
+template<class F>
+CELER_FUNCTION auto BIHIntersectingVolFinder::visit_inf_vols(
+    F&& visit_vol,
+    BIHIntersectingVolFinder::Intersection min_intersection) const -> Intersection
+{
+    for (auto i : range(tree_.inf_volids.size()))
+    {
+        auto id = storage_.local_volume_ids[tree_.inf_volids[i]];
+
+        auto intersection = visit_vol(id);
+        if (intersection.dist < min_intersection.dist)
+        {
+            min_intersection = intersection;
+        }
+    }
+    return min_intersection;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace detail
+}  // namespace celeritas
